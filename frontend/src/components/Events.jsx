0905ac58a61e83189fc060ef3ae7f20598ab342e
Nalin Angrish <nalin@nalinangrish.me>
2023-12-14 21:47:07 +0530

    Code documentation

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "../styles/componentsStyles/Events.css";
 
+/**
+ * A context to manage opening and closing the event details popup.
+ */
 const EventPopupContext = React.createContext(null);
+
+/**
+ * A context to manage opening and closing the event details popup.
+ * @param {{children: React.Component[]}} {children} The children of the context provider.
+ * @returns {React.Component} The context provider.
+ */
 function EventPopupContextProvider({ children }) {
   const [eventPopup, setEventPopup] = React.useState({});
   return (
@@ -11,6 +20,10 @@ function EventPopupContextProvider({ children }) {
   );
 }
 
+/**
+ * A popup to show complete event details.
+ * @returns {React.Component} The event details popup.
+ */
 function EventPopup() {
   const { eventPopup, setEventPopup } = React.useContext(EventPopupContext);
   if(eventPopup.title == undefined) return <></>;
@@ -30,6 +43,11 @@ function EventPopup() {
   );
 }
 
+/**
+ * A flexible, responsive grid that displays event tiles.
+ * @param {{children: React.Component[]}} {children} The children of the Events component. Should be EventTile components.
+ * @returns {React.Component} The Events component. A flexible grid that displays event tiles.
+ */
 function Events({ children }) {
   return (
     <EventPopupContextProvider>
@@ -41,6 +59,12 @@ function Events({ children }) {
   );
 }
 
+/**
+ * A tile that displays event details. May show limited details due to height constraint. 
+ * Clicking the "View Details" button will open the EventPopup component.
+ * @param {{title: string, date: string, description: string, image: string}} {title, date, description, image} The event details. 
+ * @returns {React.Component} The EventTile component. A tile that displays event details.
+ */
 function EventTile({ title, date, description, image }) {
   const { setEventPopup } = React.useContext(EventPopupContext);
   return (
