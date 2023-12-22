d71d877431c287a3c815cdfc31ae7f19aa981bd5
Krishan Kant <kkantmbd@gmail.com>
2023-12-22 16:26:06 +0530

    Formatted some files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,54 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar"
-import FooterPES from './components/Footer';
+import Navbar from "./components/Navbar";
+import FooterPES from "./components/Footer";
 
-import CelebrationsPage from "./pages/CelebrationsPage"
-import CompetitionsPage from "./pages/CompetitionsPage"
-import ContactPage from "./pages/ContactPage"
-import DonatePage from "./pages/DonatePage"
-import HomePage from "./pages/HomePage"
-import JoinUsPage from "./pages/JoinUsPage"
-import KV_IITRopar_Page from "./pages/KV_IITRopar_Page"
-import LoginPage from "./pages/LoginPage"
-import OtherEventsPage from "./pages/OtherEventsPage"
-import OurTeamPage from "./pages/OurTeamPage"
-import OutreachPage from "./pages/OutreachPage"
-import PaathshalaPage from "./pages/PaathshalaPage"
-import PastTeams from './pages/PastTeams'
-import SchoolOutreachPage from "./pages/SchoolOutreachPage"
+import CelebrationsPage from "./pages/CelebrationsPage";
+import CompetitionsPage from "./pages/CompetitionsPage";
+import ContactPage from "./pages/ContactPage";
+import DonatePage from "./pages/DonatePage";
+import HomePage from "./pages/HomePage";
+import JoinUsPage from "./pages/JoinUsPage";
+import KV_IITRopar_Page from "./pages/KV_IITRopar_Page";
+import LoginPage from "./pages/LoginPage";
+import OtherEventsPage from "./pages/OtherEventsPage";
+import OurTeamPage from "./pages/OurTeamPage";
+import OutreachPage from "./pages/OutreachPage";
+import PaathshalaPage from "./pages/PaathshalaPage";
+import PastTeams from "./pages/PastTeams";
+import SchoolOutreachPage from "./pages/SchoolOutreachPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/contact' element={<ContactPage />} />
-        <Route path='/celebrations' element={<CelebrationsPage />} />
-        <Route path='/donate' element={<DonatePage />} />
-        <Route path='/competitions' element={<CompetitionsPage />} />
-        <Route path='/joinus' element={<JoinUsPage />} />
-        <Route path='/ourteam' element={<OurTeamPage />} />
-        <Route path='/outreach' element={<OutreachPage />} />
-        <Route path='/paathshala' element={<PaathshalaPage />} />
-        <Route path='/schooloutreach' element={<SchoolOutreachPage />} />
-        <Route path='/kviitropar' element={<KV_IITRopar_Page />} />
-        <Route path='/otherevents' element={<OtherEventsPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/pastteams' element={<PastTeams />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/celebrations" element={<CelebrationsPage />} />
+        <Route path="/donate" element={<DonatePage />} />
+        <Route path="/competitions" element={<CompetitionsPage />} />
+        <Route path="/joinus" element={<JoinUsPage />} />
+        <Route path="/ourteam" element={<OurTeamPage />} />
+        <Route path="/outreach" element={<OutreachPage />} />
+        <Route path="/paathshala" element={<PaathshalaPage />} />
+        <Route path="/schooloutreach" element={<SchoolOutreachPage />} />
+        <Route path="/kviitropar" element={<KV_IITRopar_Page />} />
+        <Route path="/otherevents" element={<OtherEventsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/pastteams" element={<PastTeams />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,17 @@
-import "../styles/pagesStyles/HomePage.css"
+import "../styles/pagesStyles/HomePage.css";
 import StickyJoinButton from "../components/StickyJoinButton";
 import StickyDonateButton from "../components/StickyDonateButton";
 import WeDoCard from "../components/WhatWeDoCard";
-import homepage_about_1 from "../assets/home-page-about-1.jpg"
-import homepage_about_2 from "../assets/home-page-about-2.jpg"
-import homepage_pathshala from "../assets/home-page-our-work-pathshala.jpg"
-import homepage_pathshala_2 from "../assets/home-page-our-work-pathshala-2.jpg"
-import homepage_outreach from "../assets/home-page-our-work-outreach.jpg"
-import homepage_outreach_2 from "../assets/home-page-our-work-outreach-2.jpg"
-import homepage_others from "../assets/home-page-our-work-others.jpg"
-import homepage_others_2 from "../assets/home-page-our-work-others-2.jpg"
+import homepage_about_1 from "../assets/home-page-about-1.jpg";
+import homepage_about_2 from "../assets/home-page-about-2.jpg";
+import homepage_pathshala from "../assets/home-page-our-work-pathshala.jpg";
+import homepage_pathshala_2 from "../assets/home-page-our-work-pathshala-2.jpg";
+import homepage_outreach from "../assets/home-page-our-work-outreach.jpg";
+import homepage_outreach_2 from "../assets/home-page-our-work-outreach-2.jpg";
+import homepage_others from "../assets/home-page-our-work-others.jpg";
+import homepage_others_2 from "../assets/home-page-our-work-others-2.jpg";
 
 import FooterPES from "../components/Footer";
-
 
 import Hero from "../components/Hero";
 
@@ -22,54 +21,52 @@ import hero_image3 from "../assets/hero_image3.jpg";
 import hero_image4 from "../assets/hero_image4.jpg";
 import Navbar from "../components/Navbar";
 
-const heroSliderImages = [
-    hero_image1, hero_image2, hero_image3, hero_image4
-]; 
+const heroSliderImages = [hero_image1, hero_image2, hero_image3, hero_image4];
 
 import Testimonial from "../components/Testimonial";
 
-function HomePage(){
-    return(
-        <>
-            <Navbar />
-            <Hero 
-                heroClassName="hero"
-                heroSliderImages={heroSliderImages}
-                heroHeading="Education for underpriviledged"
-                heroParagraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit."    
-            />
-            <Testimonial />
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <Hero
+        heroClassName="hero"
+        heroSliderImages={heroSliderImages}
+        heroHeading="Education for underpriviledged"
+        heroParagraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      />
+      <Testimonial />
 
-            <section className="OurWork">
-                <h1>WHAT WE DO</h1>
-                <div className="OurWork-Content">
-                        {/* Make sure the image are in 3:4 ratio */}
-                        <WeDoCard 
-                            img = {homepage_pathshala_2} 
-                            title = "PAATHSHALA"
-                            details = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae amet nemo odio quos accusamus aliquid omnis, quam numquam temporibus, molestiae quasi sed mollitia, voluptas nesciunt sequi obcaecati soluta facere dolorem."
-                            link = "/paathshala"
-                        />
-                        <WeDoCard 
-                            img = {homepage_outreach_2} 
-                            title = "OUTREACH"
-                            details = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae amet nemo odio quos accusamus aliquid omnis, quam numquam temporibus, molestiae quasi sed mollitia, voluptas nesciunt sequi obcaecati soluta facere dolorem."
-                            link = "/outreach"
-                        />
-                        <WeDoCard 
-                            img = {homepage_others_2} 
-                            title = "EVENTS"
-                            details = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae amet nemo odio quos accusamus aliquid omnis, quam numquam temporibus, molestiae quasi sed mollitia, voluptas nesciunt sequi obcaecati soluta facere dolorem."
-                            link = "/otherevents"
-                        />
-                </div>
-            </section>
-            
-            <StickyJoinButton />
-            <StickyDonateButton />
-            <FooterPES />
-        </>
-    )
+      <section className="OurWork">
+        <h1>WHAT WE DO</h1>
+        <div className="OurWork-Content">
+          {/* Make sure the image are in 3:4 ratio */}
+          <WeDoCard
+            img={homepage_pathshala_2}
+            title="PAATHSHALA"
+            details="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae amet nemo odio quos accusamus aliquid omnis, quam numquam temporibus, molestiae quasi sed mollitia, voluptas nesciunt sequi obcaecati soluta facere dolorem."
+            link="/paathshala"
+          />
+          <WeDoCard
+            img={homepage_outreach_2}
+            title="OUTREACH"
+            details="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae amet nemo odio quos accusamus aliquid omnis, quam numquam temporibus, molestiae quasi sed mollitia, voluptas nesciunt sequi obcaecati soluta facere dolorem."
+            link="/outreach"
+          />
+          <WeDoCard
+            img={homepage_others_2}
+            title="EVENTS"
+            details="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae amet nemo odio quos accusamus aliquid omnis, quam numquam temporibus, molestiae quasi sed mollitia, voluptas nesciunt sequi obcaecati soluta facere dolorem."
+            link="/otherevents"
+          />
+        </div>
+      </section>
+
+      <StickyJoinButton />
+      <StickyDonateButton />
+      <FooterPES />
+    </>
+  );
 }
 
 export default HomePage;
