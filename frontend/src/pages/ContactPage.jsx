32f3fc95b2daabde38b1b3acffc29e83150569cb
JayGoel07 <jaygoel7@gmail.com>
2024-01-12 23:49:14 +0530

    changes contact us- join us - paathshala page

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -11,7 +11,7 @@ function ContactPage(){
              />
             <div class="text1s">
                 <div class="quote1"><b>"The best way to find yourself is to lose yourself in the     service of others"   </b></div>
-                <div class="quote2">       BY  Mahatma Gandhi</div>
+                <div class="quote2">   ~                     BY  Mahatma Gandhi</div>
             </div>
             <div class="form">
             <form> 
