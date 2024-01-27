abd26bf2bd8b63158c01388597eeadf7e3ee6170
Krishan Kant <kkantmbd@gmail.com>
2024-01-28 01:57:50 +0530

    removed testimonial picture temp

--- frontend/src/components/Testimonial2.jsx
+++ frontend/src/components/Testimonial2.jsx
@@ -3,7 +3,7 @@ import "../styles/componentsStyles/Testimonial2.css"
 function Testimonial2(props){
     return(
         <div className="testimonial2">
-            <img src={props.img} className="testimonial2-img"></img>
+            {/* <img src={props.img} className="testimonial2-img"></img> */}
             <div className="testimonial2-content">
                 <p className="testimonial2-p">"{props.description}"</p>
                 <h2 className="testimonial2-name">{props.name}</h2>
