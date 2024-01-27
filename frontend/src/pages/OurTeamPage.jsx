466cb727534a82e58d708d140577d822297c6113
Jay Goel <147596613+JayGoel07@users.noreply.github.com>
2024-01-27 18:04:53 +0530

    Jaygoel branch (#42)
    * Update ContactPage.css
    
    * Create Carousel.jsx
    
    * Update PaathshalaPage.jsx
    
    * Create Carousel.css
    
    * Add files via upload
    
    * removed typo
    
    * changed contactus- donateus- join us page as our team page
    
    * changed joinus and contact-us page
    
    * changes contact us- join us - paathshala page
    
    * some already loopholes.. pls check before merge
    
    * updated what we do and about page
    
    * completed testimonials secion + half done Home page
    
    * half home + testimonial + webdev
    
    * complete frontend
    
    ---------
    
    Co-authored-by: Krishan Kant <kkantmbd@gmail.com>

--- frontend/src/pages/OurTeamPage.jsx
+++ frontend/src/pages/OurTeamPage.jsx
@@ -2,6 +2,13 @@ import { Link } from 'react-router-dom';
 import "../styles/pagesStyles/OurTeamPage.css"
 import TeamsCard from "../components/TeamsCard"
 import profilepic from "../assets/governingbody/president.png"
+import webdev_1 from "../assets/webdevelopers/webdev_1.jpg"
+import webdev_2 from "../assets/webdevelopers/webdev_2.jpg"
+import webdev_3 from "../assets/webdevelopers/webdev_3.jpg"
+import webdev_4 from "../assets/webdevelopers/webdev_4.jpg"
+import webdev_5 from "../assets/webdevelopers/webdev_5.jpg"
+import webdev_6 from "../assets/webdevelopers/webdev_6.jpg"
+import webdev_7 from "../assets/webdevelopers/webdev_7.jpg"
 import PageHeader from '../components/PageHeader';
 import HeaderImage from "../assets/header-testing.jpg"
 
@@ -19,21 +26,27 @@ function OurTeamPage(){
                     <div className="Cards">
                         <TeamsCard
                             img = {profilepic}
-                            name = "Prof. Rajeev Ahuja"
-                            designation = "Director, IIT Ropar"
-                            // email = "2021meb1322@iitrpr.ac.in"
+                            name = "Shivam"
+                            designation = "President"
+                            email = "2021meb1322@iitrpr.ac.in"
                         />
                         <TeamsCard
                             img = {profilepic}
-                            name = "Dr. Pushpendra P. Singh"
-                            designation = "Associate Professor, IIT Ropart"
-                            // email = "2021csb1137@iitrpr.ac.in"
+                            name = "Yashaswini Vajja"
+                            designation = "Vice-President"
+                            email = "2021csb1137@iitrpr.ac.in"
                         />
                         <TeamsCard
                             img = {profilepic}
-                            name = "Prof. Harpreet Singh"
-                            designation = "Professor, IIT Ropar"
-                            // email = "2021eeb1204@iitrpr.ac.in"
+                            name = "Reetika"
+                            designation = "Vice-President SI"
+                            email = "2021eeb1204@iitrpr.ac.in"
+                        />
+                        <TeamsCard
+                            img = {profilepic}
+                            name = "Nehal Sonkar"
+                            designation = "Secretary"
+                            email = "2021meb1304@iitrpr.ac.in"
                         />
                  
                     </div>
@@ -121,22 +134,46 @@ function OurTeamPage(){
                     <h1>Web Developers</h1>
                     <div className="Cards">
                         <TeamsCard
-                            img = {profilepic}
-                            name = "Mr. Catinson"
-                            designation = "President"
-                            email = "sample@email.com"
+                            img = {webdev_1}
+                            name = "Krishan Kant"
+                            designation = "Front-end Lead"
+                            email = "2021meb1293@iitrpr.ac.in"
                         />
                         <TeamsCard
-                            img = {profilepic}
-                            name = "Mr. Catinson"
-                            designation = "President"
-                            email = "sample@email.com"
+                            img = {webdev_2}
+                            name = "Meriga.Sai Lahari "
+                            designation = "Back-end Lead"
+                            email = "2023eem1006@iitrpr.ac.in"
                         />
                         <TeamsCard
-                            img = {profilepic}
-                            name = "Mr. Catinson"
-                            designation = "President"
-                            email = "sample@email.com"
+                            img = {webdev_3}
+                            name = "Upasna"
+                            designation = "Back-end Team"
+                            email = "2019csm1015@iitrpr.ac.in"
+                        />
+                        <TeamsCard
+                            img = {webdev_4}
+                            name = "Harshit Kumar"
+                            designation = "Back-end Team"
+                            email = "2021csb1093@iitrpr.ac.in"
+                        />
+                        <TeamsCard
+                            img = {webdev_5}
+                            name = "Gopi Varshini"
+                            designation = "Front-end Team"
+                            email = "2022mcb1264@iitrpr.ac.in"
+                        />
+                        <TeamsCard
+                            img = {webdev_6}
+                            name = "Jay Goel"
+                            designation = "Front-end Team"
+                            email = "2022eeb1178@iitrpr.ac.in"
+                        />
+                        <TeamsCard
+                            img = {webdev_7}
+                            name = "Nalin Angrish"
+                            designation = "Back-end Team"
+                            email = "2023meb1360@iitrpr.ac.in"
                         />
                     </div>
                 </section>
